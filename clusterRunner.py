6e8d6d77d69ebbf3b01e1a656066745e29eca693
Samuel Witte <witte.sam@gmail.com>
2018-02-08 18:48:14 +0100

    parallelized

--- clusterRunner.py
+++ clusterRunner.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from CMB import *
+from multiprocessing import Pool
 
 Ftag = 'StandardUniverse'
 lmax_Pert = 5
@@ -12,9 +13,9 @@ compute_MPS = True
 
 kmin = 1e-3
 kmax = 0.5
-knum = 200
+knum = 20
 lmax = 2500
-lvals = 250
+lvals = 20
 
 OM_b = 0.0484
 OM_c = 0.258 
@@ -24,12 +25,22 @@ OM_L = 0.7 # Doesnt matter, it calculates with flat Uni
 
 kgrid = np.logspace(np.log10(kmin), np.log10(kmax), knum)
 
-if compute_LP or compute_TH:
-    for k in kgrid:
-        CMB(OM_b, OM_c, OM_g, OM_L, kmin=kmin, kmax=kmax, knum=knum, lmax=lmax, 
-            lvals=lvals, compute_LP=True, compute_TH=True, 
-            compute_CMB=False, compute_MPS=False, kVAL=k, 
+def CMB_wrap(kval):
+    CMB(OM_b, OM_c, OM_g, OM_L, kmin=kmin, kmax=kmax, knum=knum, lmax=lmax,
+            lvals=lvals, compute_LP=True, compute_TH=True,
+            compute_CMB=False, compute_MPS=False, kVAL=kval,
             Ftag=Ftag, lmax_Pert=lmax_Pert)
+    return
+
+if compute_LP or compute_TH:
+    pool = Pool(processes=None)
+    pool.map(CMB_wrap, kgrid)
+    
+#    for k in kgrid:
+#        CMB(OM_b, OM_c, OM_g, OM_L, kmin=kmin, kmax=kmax, knum=knum, lmax=lmax,
+#            lvals=lvals, compute_LP=True, compute_TH=True,
+#            compute_CMB=False, compute_MPS=False, kVAL=k,
+#            Ftag=Ftag, lmax_Pert=lmax_Pert)
 
 if compute_CMB:
     CMB(OM_b, OM_c, OM_g, OM_L, kmin=kmin, kmax=kmax, knum=knum, lmax=lmax,
@@ -39,3 +50,4 @@ if compute_MPS:
     CMB(OM_b, OM_c, OM_g, OM_L, kmin=kmin, kmax=kmax, knum=knum, lmax=lmax,
         lvals=lvals, compute_LP=False, compute_TH=False, compute_CMB=False,
         compute_MPS=True, kVAL=k, Ftag=Ftag, lmax_Pert=lmax_Pert)
+
