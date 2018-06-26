9af316d224c363826e6990044163de49351c98c4
Samuel Witte <witte.sam@gmail.com>
2018-06-26 14:47:16 +0200

    post theta anlaysis

--- check_theta_RT.py
+++ check_theta_RT.py
@@ -3,16 +3,27 @@ import os
 import glob
 
 path = os.getcwd()
-files = path + '/OutputFiles/*_ThetaFile_kval*.dat'
+
+inRUN = False
+
+if inRUN:
+    files = path + '/OutputFiles/*_ThetaFile_kval*.dat'
+else:
+    files = path + '/OutputFiles/StanardUniverse_ThetaCMB_Table.dat'
 
 ell_indx = 0
 endarr = []
 
 file_list = glob.glob(files)
 for ff in file_list:
-    kval = float(ff[ff.find('_kval_')+6:ff.find('.dat')])
-    loadf = np.loadtxt(ff)[ell_indx]
-    endarr.append([kval, loadf])
-    
-finArr = np.asarray(endarr)[np.argsort(np.asarray(endarr)[:,0])]
+    if inRUN:
+        kval = float(ff[ff.find('_kval_')+6:ff.find('.dat')])
+        loadf = np.loadtxt(ff)[ell_indx]
+        endarr.append([kval, loadf])
+    else:
+        finArr = np.loadtxt(ff)[1:, ell_indx]
+
+if inRUN:
+    finArr = np.asarray(endarr)[np.argsort(np.asarray(endarr)[:,0])]
+
 np.savetxt(path + '/OutputFiles/CHECK_THETA.dat', finArr)
